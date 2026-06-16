ead29ebfff527e8e0fb194d7ad63e3e85c8598d2
shaanpatel-ai <shaanpatelnz@gmail.com>
2026-06-16 18:57:19 +1200

    intial commit

--- My first website/script.js
+++ My first website/script.js
@@ -1,14 +1,13 @@
 const images = [
     
     "https://c4.wallpaperflare.com/wallpaper/411/115/636/1920x1200-space-wallpaper-preview.jpg",
-    "https://gallery.yopriceville.com/downloadfullsize/send/8797",
-    "https://wallpapers.com/images/featured/space-sjryfre8k8f6i3ge.jpg"
+    " https://wallpaperaccess.com/full/8110241.jpg",
+  "https://gallery.yopriceville.com/downloadfullsize/send/8797"
 ];
 let i = 0;
 setInterval(function ()  {
 if (i === images.length) {
-    i = 0
+    i = 0;
 }
-    document.getElementById( "slide").src = images[i]
-} ,3000
-);
+    document.getElementById("slide").src = images[i];
+} ,3000);
